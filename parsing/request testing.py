fab50e2cf6b1001d48f5e66d44af7b65ecc55646
Asi <as@>
2014-01-06 03:59:09 +0200

    random fuckups

--- parsing/request testing.py
+++ parsing/request testing.py
@@ -1,1 +1,14 @@
 __author__ = 'Asi'
+
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("http://oreilly.com/store/samplers.html")
+
+c = r.content
+soup = BeautifulSoup(c)
+samples = soup.find_all("a", "item-title")
+
+count = 0
+for i in samples:
+    print i
