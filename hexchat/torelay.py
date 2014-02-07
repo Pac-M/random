76be741a90a3c890f21c7221173cdd3307cac2fd
Asi <as@>
2014-02-07 13:44:11 +0200

    updated color output

--- hexchat/torelay.py
+++ hexchat/torelay.py
@@ -49,14 +49,14 @@ def return_string(word, source):
     if name:#HD-T
         string = "%s %s %s %s %s" % (name.group(2), name.group(3), name.group(1), name.group(4), name.group(5))
         write_file(string)
-        string = "\00309%s \00304%s \017%s %s %s %s" % (source, name.group(2), name.group(3), name.group(1), name.group(4), name.group(5))
+        string = "\00309[%s] \00304%s \017%s %s %s %s" % (source, name.group(2), name.group(3), name.group(1), name.group(4), name.group(5))
         return string
     #GFT
     name = re.search(r"NEW 4::7 ([A-Za-z0-9 .-]*)\b 4::3 (Movies/X264-HD|Movies/XVID|Movies/X264-SD|Movies/DVDR|Movies/BLURAY) 4::3 (\S*)", word)
     if name: #gft
         string = "%s %s" % (name.group(1), name.group(3))
         write_file(string)
-        string = "\00310%s \00304%s \017%s" % (source, name.group(1), name.group(3))
+        string = "\00310[%s] \00304%s \017%s" % (source, name.group(1), name.group(3))
         return string
     #BitHQ
     name = re.search(r"(DVD-R/Movies|DVD-R/Asian Cinema|High Quality) - (.*) \((\d{4})\) ([a-zA-Z0-9 /]+\b)  - (.*)",
@@ -64,14 +64,14 @@ def return_string(word, source):
     if name:
         string = "%s %s %s %s" % (name.group(2), name.group(3), name.group(4), name.group(5))
         write_file(string)
-        string = "\00311%s \00304%s \017%s %s %s" % (source, name.group(2), name.group(3), name.group(4), name.group(5))
+        string = "\00311[%s] \00304%s \017%s %s %s" % (source, name.group(2), name.group(3), name.group(4), name.group(5))
         return string
     #TSH
     name = re.search(r"Movies/\S* :: (.*) :: (http\S*)", word)
     if name:
         string = "%s %s" % (name.group(1), name.group(2))
         write_file(string)
-        string = "\00312%s \00304%s \017%s" % (source, name.group(1), name.group(2))
+        string = "\00312[%s] \00304%s \017%s" % (source, name.group(1), name.group(2))
         return string
     return word
 
@@ -83,7 +83,7 @@ def echo_cb(word, word_eol, user_data):
         cont = hexchat.find_context(channel=target)
         reg = return_string(word[1], source)
         #output = reg + " \037\00304was announced on: " + source
-        output = "[" + source + "] " + reg
-        cont.command("say %s" % output)
+        #output = "[" + source + "] " + reg
+        cont.command("say %s" % reg)
 
 hexchat.hook_print("Channel Message", echo_cb)
